7e481198a603338da04ec9a8205e130160353031
Jurvis Tan <jurvis.tan@me.com>
2016-12-04 15:02:25 -0800

    fix marking of post as published

--- web/views/admin/shared_view.ex
+++ web/views/admin/shared_view.ex
@@ -1,6 +1,22 @@
 defmodule Louvre.Admin.SharedView do
   use Phoenix.HTML
 
+  def semantic_datetime_select(form, field, opts \\ []) do
+    builder = fn b ->
+      ~e"""
+      <div class="fields">
+        <div class="three wide field"><%= b.(:month, []) %></div>
+        <div class="two wide field"><%= b.(:day, []) %></div>
+        <div class="two wide field"><%= b.(:year, [options: 2008..2025]) %></div> at
+        <div class="two wide field"><%= b.(:hour, []) %></div>:
+        <div class="two wide field"><%= b.(:minute, []) %></div>&nbsp;UTC
+      </div>
+      """
+    end
+
+    datetime_select form, field, [builder: builder] ++ opts
+  end
+
   def error_class(form, field) do
     if form.errors[field], do: "error", else: ""
   end
